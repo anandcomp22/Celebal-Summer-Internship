7eba3999fbb10ba68d849a10a52897ef86e76166
Anand <moreanand111011@gmail.com>
2025-07-03 00:23:00 +0530

    env add to hide password

--- CRUD_SQL/db.js
+++ CRUD_SQL/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
+require("dotenv").config();
 
 const mysqlPool = mysql.createPool({
     host:'localhost',
     user:'root',
-    password:'455169@nN123',
+    password: process.env.PASSWORD,
     database: 'employee_db'
 })
 
